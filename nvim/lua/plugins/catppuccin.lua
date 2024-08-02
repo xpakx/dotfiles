217eb70dc5ddf7f5bc14cd67c19066f870886802
xpakx <55586470+xpakx@users.noreply.github.com>
2024-08-02 13:43:24 +0200

    simplify theme configuration

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -2,11 +2,5 @@ return {
 	"catppuccin/nvim", 
 	name = "catppuccin",
 	lazy = false,
-	config = function()
-		vim.cmd('colorscheme catppuccin')
-		require("catppuccin").setup {
-			color_overrides = {
-			}
-		}
-	end
 }
+
