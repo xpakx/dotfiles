87849b048f6a4579908ea2fbcc8ba2fe9ea555f4
xpakx <55586470+xpakx@users.noreply.github.com>
2024-08-12 02:59:19 +0200

    decouple lsp keybindings from lsp-zero

--- nvim/lua/plugins/lsp-zero.lua
+++ nvim/lua/plugins/lsp-zero.lua
@@ -28,20 +28,6 @@ return {
 				['<CR>'] = cmp.mapping.confirm({ select = true }),
 			}
 		})
-
-		lsp.on_attach(function(client, bufnr)
-			local opts = {buffer = bufnr, remap = false}
-
-			vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-			vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-			vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
-			vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
-			vim.keymap.set("n", "<leader>lc", function() vim.lsp.buf.code_action() end, opts)
-			vim.keymap.set("n", "<leader>lR", function() vim.lsp.buf.references() end, opts)
-			vim.keymap.set("n", "<leader>lr", function() vim.lsp.buf.rename() end, opts)
-			vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-		end)
-
 		lsp.setup()
 	end
 }
--- nvim/lua/xpakx/remap.lua
+++ nvim/lua/xpakx/remap.lua
@@ -10,3 +10,21 @@ vim.keymap.set("n", "<M-l>", ':wincmd l<CR>')
 
 vim.keymap.set("n", "[q", ':cp!<cr>')
 vim.keymap.set("n", "]q", ':cn!<cr>')
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP keybindings',
+  callback = function(event)
+    local opts = {buffer = event.buf}
+
+    -- vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    -- vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>lc", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>lR", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>lr", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  end
+})
