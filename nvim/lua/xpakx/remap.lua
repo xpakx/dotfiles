027cad3bb839c4c993d17ca6b9996747cff95bdf
xpakx <55586470+xpakx@users.noreply.github.com>
2025-03-02 16:47:53 +0100

    feat: add script for downloading url as markdown file

--- nvim/lua/xpakx/remap.lua
+++ nvim/lua/xpakx/remap.lua
@@ -16,8 +16,8 @@ vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(event)
     local opts = {buffer = event.buf}
 
-    -- vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    -- vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
     vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
